44f2aba3b9b09407dbbdeaa06203c103575ac85c
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2015-08-01 11:57:05 +0200

    Base widget (just showing the five icons)

---
+++ RpslsWidget.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace mauriziocingolani\yii2fmwkrpsls;
+
+use yii\base\Widget;
+
+class RpslsWidget extends Widget {
+
+    public function run() {
+        return $this->render('rpslsWidget');
+    }
+
+}
---
+++ views/rpslsWidget.php
@@ -0,0 +1,9 @@
+<?php ?>
+
+<div class="rpsls">
+    <a href="" onclick="return false"><i class="fa fa-hand-rock-o" title="<?= Yii::t('rpsls', 'Rock'); ?>"></i></a>
+    <a href="" onclick="return false"><i class="fa fa-hand-paper-o" title="<?= Yii::t('rpsls', 'Paper'); ?>"></i></a>
+    <a href="" onclick="return false"><i class="fa fa-hand-scissors-o" title="<?= Yii::t('rpsls', 'Scissors'); ?>"></i></a>
+    <a href="" onclick="return false"><i class="fa fa-hand-lizard-o" title="<?= Yii::t('rpsls', 'Lizard'); ?>"></i></a>
+    <a href="" onclick="return false"><i class="fa fa-hand-spock-o" title="<?= Yii::t('rpsls', 'Spock'); ?>"></i></a>
+</div>
